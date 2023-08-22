2052e59ef29c3486e549940211a7f37e38182449
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-08-22 22:40:02 +0530

    feat: linting

--- components/AuthModel.tsx
+++ components/AuthModel.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import {
-  useSessionContext,
-  useSupabaseClient,
+    useSessionContext,
+    useSupabaseClient,
 } from "@supabase/auth-helpers-react";
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
@@ -12,47 +12,47 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { use, useEffect } from "react";
 
 export default function AuthModal() {
-  const supabaseClient = useSupabaseClient();
-  const router = useRouter();
-  const { session } = useSessionContext();
-  const { close, isOpen } = useAuthModal();
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const { session } = useSessionContext();
+    const { close, isOpen } = useAuthModal();
 
-  useEffect(() => {
-    if (session) {
-      router.refresh();
-      close();
-    }
-  }, [session, router, close]);
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            close();
+        }
+    }, [session, router, close]);
 
-  const onChange = (open: Boolean) => {
-    if (!open) {
-      close();
-    }
-  };
-  return (
-    <Modal
-      title="Welcome back!"
-      description="Sign in to your account to continue."
-      isOpen={isOpen}
-      onChange={onChange}
-    >
-      <Auth
-        theme="dark"
-        magicLink
-        providers={["github"]}
-        supabaseClient={supabaseClient}
-        appearance={{
-          theme: ThemeSupa,
-          variables: {
-            default: {
-              colors: {
-                brand: "#404040",
-                brandAccent: "#22c55e",
-              },
-            },
-          },
-        }}
-      />
-    </Modal>
-  );
+    const onChange = (open: Boolean) => {
+        if (!open) {
+            close();
+        }
+    };
+    return (
+        <Modal
+            title="Welcome back!"
+            description="Sign in to your account to continue."
+            isOpen={isOpen}
+            onChange={onChange}
+        >
+            <Auth
+                theme="dark"
+                magicLink
+                providers={["github"]}
+                supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: "#404040",
+                                brandAccent: "#22c55e",
+                            },
+                        },
+                    },
+                }}
+            />
+        </Modal>
+    );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,50 +11,50 @@ import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
 import { toast } from "react-hot-toast";
 interface HeaderProps {
-  children: React.ReactNode;
-  className?: string;
+    children: React.ReactNode;
+    className?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
-  const AuthModal = useAuthModal();
-  const router = useRouter();
-  const supabaseClient = useSupabaseClient();
-  const { user } = useUser();
-  const handleLogOut = async () => {
-    const { error } = await supabaseClient.auth.signOut();
-    // todo:reset any playing songs
-    router.refresh();
-    if (error) {
-      toast.error(error.message);
-    } else {
-      toast.success("Logged out successfully");
-    }
-  };
-  return (
-    <div
-      className={twMerge(
-        ` h-fit bg-gradient-to-b from-emerald-800 p-6`,
-        className
-      )}
-    >
-      <div className="w-full mb-4  flex items-center justify-between">
-        <div className="hidden md:flex gap-x-2 items-center">
-          <button
-            onClick={() => router.back()}
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
-          >
-            <RxCaretLeft className="text-white" size={35} />
-          </button>
-          <button
-            onClick={() => router.forward()}
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
-          >
-            <RxCaretRight className="text-white" size={35} />
-          </button>
-        </div>
-        <div className="flex md:hidden gap-x-2 items-center">
-          <button
-            className="
+    const AuthModal = useAuthModal();
+    const router = useRouter();
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+    const handleLogOut = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        // todo:reset any playing songs
+        router.refresh();
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success("Logged out successfully");
+        }
+    };
+    return (
+        <div
+            className={twMerge(
+                ` h-fit bg-gradient-to-b from-emerald-800 p-6`,
+                className
+            )}
+        >
+            <div className="w-full mb-4  flex items-center justify-between">
+                <div className="hidden md:flex gap-x-2 items-center">
+                    <button
+                        onClick={() => router.back()}
+                        className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+                    >
+                        <RxCaretLeft className="text-white" size={35} />
+                    </button>
+                    <button
+                        onClick={() => router.forward()}
+                        className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+                    >
+                        <RxCaretRight className="text-white" size={35} />
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button
+                        className="
             rounded-full
             bg-white
             flex
@@ -64,11 +64,11 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             transition
             p-2
           "
-          >
-            <HiHome className="text-black" size={20} />
-          </button>
-          <button
-            className="
+                    >
+                        <HiHome className="text-black" size={20} />
+                    </button>
+                    <button
+                        className="
             rounded-full
             bg-white
             flex
@@ -78,45 +78,45 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             transition
             p-2
           "
-          >
-            <BiSearch className="text-black" size={20} />
-          </button>
-        </div>
-        <div className="flex items-center justify-center gap-x-4">
-          {user ? (
-            <div className="flex gap-x-4 items-center">
-              <Button onClick={handleLogOut} className="bg-white px-6 py-2">
-                Logout
-              </Button>
-              <Button
-                onClick={() => router.push("/account")}
-                className="bg-white"
-              >
-                <FaUserAlt size={20} />
-              </Button>
+                    >
+                        <BiSearch className="text-black" size={20} />
+                    </button>
+                </div>
+                <div className="flex items-center justify-center gap-x-4">
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
+                            <Button onClick={handleLogOut} className="bg-white px-6 py-2">
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push("/account")}
+                                className="bg-white"
+                            >
+                                <FaUserAlt size={20} />
+                            </Button>
+                        </div>
+                    ) : (
+                        <>
+                            <div>
+                                <Button
+                                    onClick={AuthModal.open}
+                                    className="bg-transparent text-neutral-300 font-medium"
+                                >
+                                    Sign up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button onClick={AuthModal.open} className="bg-white px-6 py-2">
+                                    Login in
+                                </Button>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
-          ) : (
-            <>
-              <div>
-                <Button
-                  onClick={AuthModal.open}
-                  className="bg-transparent text-neutral-300 font-medium"
-                >
-                  Sign up
-                </Button>
-              </div>
-              <div>
-                <Button onClick={AuthModal.open} className="bg-white px-6 py-2">
-                  Login in
-                </Button>
-              </div>
-            </>
-          )}
+            {children}
         </div>
-      </div>
-      {children}
-    </div>
-  );
+    );
 };
 
 export default Header;
