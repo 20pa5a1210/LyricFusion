ee30b07c56a7350f3e9bbd4f1312f7a19eedc4d2
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-08-30 00:28:40 +0530

    feat: songs fetching and searching functionality

---
+++ actions/getSongs.ts
@@ -0,0 +1,20 @@
+import { Song } from "@/types";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export const getSongs = async (): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies
+    })
+
+    const { data, error } = await supabase
+        .from('songs')
+        .select('*')
+        .order('created_at', { ascending: false })
+
+    if (error) {
+        console.log(error)
+    }
+
+    return (data as any) || []
+}
