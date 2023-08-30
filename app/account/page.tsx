9089f194bb64fa3a82c204dbc78a068da952f99d
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-08-30 18:45:28 +0530

    feat: account page

---
+++ app/account/page.tsx
@@ -0,0 +1,41 @@
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+
+
+const Account = () => {
+    return (
+        <div
+            className="
+        bg-neutral-900
+        rounded-lg
+        h-full
+        w-full
+        overflow-hidden
+        overflow-y-auto
+      "
+        >
+            <Header className="from-bg-neutral-900">
+                <div className="mb-2 flex flex-col gap-y-6">
+                    <h1 className="text-white text-3xl font-semibold">
+                        Account Settings
+                    </h1>
+                </div>
+            </Header>
+            <div className="mb-7 px-6">
+                <div className="flex flex-col gap-y-4">
+                    <p>You are currently on the
+                        free plan.
+                    </p>
+                    <Button
+                        disabled
+                        className="w-[300px] bg-neutral-400"
+                    >
+                        Stripe Integration Pending
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Account;
